170c7cfe39f2f7e6a4aa9d95363359c0708dbf4b
BoringCrypto <b@rtje.net>
2021-12-21 11:03:30 +0700

    Remove hooks

--- contracts/mocks/BentoBoxMock.sol
+++ contracts/mocks/BentoBoxMock.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
-import "../BentoBox.sol";
+import "../BoringBox.sol";
 
-contract BentoBoxMock is BentoBox {
+contract BentoBoxMock is BoringBox {
     constructor(IERC20 weth) public BentoBox(weth) {
         return;
     }
