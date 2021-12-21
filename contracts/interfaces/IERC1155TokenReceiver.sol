f6762e6c60729cac4777e6b6399f954dc48b3d15
BoringCrypto <b@rtje.net>
2021-12-21 15:59:01 +0700

    Partial

---
+++ contracts/interfaces/IERC1155TokenReceiver.sol
@@ -0,0 +1,17 @@
+interface IERC1155TokenReceiver {
+    function onERC1155Received(
+        address _operator,
+        address _from,
+        uint256 _id,
+        uint256 _value,
+        bytes calldata _data
+    ) external returns (bytes4);
+
+    function onERC1155BatchReceived(
+        address _operator,
+        address _from,
+        uint256[] calldata _ids,
+        uint256[] calldata _values,
+        bytes calldata _data
+    ) external returns (bytes4);
+}
